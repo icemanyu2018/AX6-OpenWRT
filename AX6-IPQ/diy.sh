ad5d29d2ef6fd67ab1f755db89bdb3bd1be8e289
icemanyu2018 <45337728+icemanyu2018@users.noreply.github.com>
2026-08-22 05:18:55 +0800

    Update diy.sh

--- AX6-IPQ/diy.sh
+++ AX6-IPQ/diy.sh
@@ -9,47 +9,51 @@ function git_sparse_clone() {
   cd .. && rm -rf $repodir
 }
 
-# Add packages
-#添加科学上网源
-#git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall-packages
-#git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
+# 1. 添加第三方插件与主题源码
 git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
-#git clone --depth 1 https://github.com/sirpdboy/luci-app-ddns-go package/ddnsgo
-#git clone --depth 1 https://github.com/sbwml/luci-app-mosdns package/mosdns
-#git clone --depth 1 https://github.com/sbwml/luci-app-alist package/alist
-#git clone --depth=1  https://github.com/kenzok8/small-package package/small-package
-#git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-zerotier
-#git_sparse_clone main https://github.com/kiddin9/kwrt-packages vlmcsd
-#git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-vlmcsd
-#git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-socat
 
-# 替换luci-app-openvpn-server imm源的启动不了服务！
-#rm -rf feeds/luci/applications/luci-app-openvpn-server
-#git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-openvpn-server
-# 调整 openvpn-server 到 VPN 菜单
-#sed -i 's/services/vpn/g' package/luci-app-openvpn-server/luasrc/controller/*.lua
-#sed -i 's/services/vpn/g' package/luci-app-openvpn-server/luasrc/model/cbi/openvpn-server/*.lua
-#sed -i 's/services/vpn/g' package/luci-app-openvpn-server/luasrc/view/openvpn/*.htm
+# 添加 daed / dae 源码
+git clone --depth 1 https://github.com/sbwml/luci-app-daed package/daed
 
-#git clone -b js https://github.com/papagaye744/luci-theme-design package/luci-theme-design
-
-#替换luci-app-socat为https://github.com/chenmozhijin/luci-app-socat
-#rm -rf feeds/luci/applications/luci-app-socat
-#git_sparse_clone main https://github.com/chenmozhijin/luci-app-socat luci-app-socat
-
-#删除库中的插件，使用自定义源中的包。
+# 2. 清理官方 feed 中可能冲突的主题包
 rm -rf feeds/luci/themes/luci-theme-argon
 rm -rf feeds/luci/applications/luci-app-argon-config
-#rm -rf feeds/luci/applications/luci-app-ddns-go
-#rm -rf feeds/packages/net/ddns-go
-#rm -rf feeds/packages/net/alist
-#rm -rf feeds/luci/applications/luci-app-alist
-#rm -rf feeds/luci/applications/openwrt-passwall
 
+# 3. 设置默认管理 IP: 192.168.1.1
+sed -i 's/192.168.[0-9]*.1/192.168.1.1/g' package/base-files/files/bin/config_generate
 
-#修改默认IP
-#sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
+# 4. 设置默认密码为空 (清除 root 默认 hash)
+sed -i 's/root:::0:99999:7:::/root::0:99999:7:::/g' package/base-files/files/etc/shadow 2>/dev/null || true
 
-#修改主机名
-#sed -i "s/hostname='ImmortalWrt'/hostname='Redmi-AX6'/g" package/base-files/files/bin/config_generate
+# 5. 设置默认 WiFi (2.4G: redmi-ax6-2.4g, 5G: redmiax6-5g, 密码: 123456789)
+mkdir -p package/base-files/files/etc/uci-defaults
+cat > package/base-files/files/etc/uci-defaults/99-custom-wifi << 'EOF'
+#!/bin/sh
+
+# 开启无线并设置 SSID 与密码
+uci -q batch << 'UCIBATCH'
+set wireless.@wifi-device[0].disabled='0'
+set wireless.@wifi-device[1].disabled='0'
+UCIBATCH
+
+# 针对具体频段精准设置
+for iface in $(uci show wireless | grep '=wifi-iface' | cut -d'.' -f2 | cut -d'=' -f1); do
+    device=$(uci -q get wireless.${iface}.device)
+    band=$(uci -q get wireless.${device}.band)
+    
+    uci -q set wireless.${iface}.disabled='0'
+    uci -q set wireless.${iface}.encryption='psk2+ccmp'
+    uci -q set wireless.${iface}.key='123456789'
+    
+    if [ "$band" = "5g" ] || [ "$device" = "radio1" ]; then
+        uci -q set wireless.${iface}.ssid='redmiax6-5g'
+    else
+        uci -q set wireless.${iface}.ssid='redmi-ax6-2.4g'
+    fi
+done
+
+uci commit wireless
+exit 0
+EOF
+chmod +x package/base-files/files/etc/uci-defaults/99-custom-wifi
