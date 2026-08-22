30374a9bfa7c10f6319237a023d86c8e77076e61
icemanyu2018 <45337728+icemanyu2018@users.noreply.github.com>
2026-08-22 11:22:45 +0800

    Update diy.sh

--- AX6-IPQ/diy.sh
+++ AX6-IPQ/diy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# Git稀疏克隆，只克隆指定目录到本地
+
+# Git 稀疏克隆函数
 function git_sparse_clone() {
   branch="$1" repourl="$2" && shift 2
   git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
@@ -9,21 +10,31 @@ function git_sparse_clone() {
   cd .. && rm -rf $repodir
 }
 
-# 1. 添加第三方插件与主题源码
+# 1. 添加第三方主题与应用源
 git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
 
 # 添加 daed / dae 源码
 git clone --depth 1 https://github.com/sbwml/luci-app-daed package/daed
 
-# 2. 清理官方 feed 中可能冲突的主题包
+# 添加 OpenClash 源码
+git clone --depth 1 -b master https://github.com/vernesong/OpenClash package/luci-app-openclash
+
+# 添加 AdGuardHome 插件源码
+git clone --depth 1 https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
+
+# 添加 wolultra 唤醒插件源码
+git clone --depth 1 https://github.com/sundaqiang/openwrt-packages/tree/master/luci-app-wolultra package/luci-app-wolultra 2>/dev/null || true
+
+# 2. 清理官方 feed 中可能冲突的旧包
 rm -rf feeds/luci/themes/luci-theme-argon
 rm -rf feeds/luci/applications/luci-app-argon-config
+rm -rf feeds/luci/applications/luci-app-openclash
 
-# 3. 设置默认管理 IP: 192.168.1.1
+# 3. 设置默认管理 IP 为 192.168.1.1
 sed -i 's/192.168.[0-9]*.1/192.168.1.1/g' package/base-files/files/bin/config_generate
 
-# 4. 设置默认密码为空 (清除 root 默认 hash)
+# 4. 设置默认 root 密码为空 (清除 root 密码哈希)
 sed -i 's/root:::0:99999:7:::/root::0:99999:7:::/g' package/base-files/files/etc/shadow 2>/dev/null || true
 
 # 5. 设置默认 WiFi (2.4G: redmi-ax6-2.4g, 5G: redmiax6-5g, 密码: 123456789)
@@ -31,13 +42,13 @@ mkdir -p package/base-files/files/etc/uci-defaults
 cat > package/base-files/files/etc/uci-defaults/99-custom-wifi << 'EOF'
 #!/bin/sh
 
-# 开启无线并设置 SSID 与密码
+# 开启物理无线接口
 uci -q batch << 'UCIBATCH'
 set wireless.@wifi-device[0].disabled='0'
 set wireless.@wifi-device[1].disabled='0'
 UCIBATCH
 
-# 针对具体频段精准设置
+# 遍历 wifi-iface 进行精确配置
 for iface in $(uci show wireless | grep '=wifi-iface' | cut -d'.' -f2 | cut -d'=' -f1); do
     device=$(uci -q get wireless.${iface}.device)
     band=$(uci -q get wireless.${device}.band)
