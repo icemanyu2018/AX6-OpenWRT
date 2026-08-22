60012a8067cfb9b897193a2d81e72c2064ab0e5c
icemanyu2018 <45337728+icemanyu2018@users.noreply.github.com>
2026-08-22 12:36:19 +0800

    Update diy.sh

--- AX6-IPQ/diy.sh
+++ AX6-IPQ/diy.sh
@@ -1,32 +1,23 @@
 #!/bin/bash
-# Git稀疏克隆，只克隆指定目录到本地
-function git_sparse_clone() {
-  branch="$1" repourl="$2" && shift 2
-  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
-  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
-  cd $repodir && git sparse-checkout set $@
-  mv -f $@ ../package
-  cd .. && rm -rf $repodir
-}
 
-# 1. 从 kenzok8/small 源中按需拉取所需插件
-git_sparse_clone main https://github.com/kenzok8/small luci-app-openclash
-git_sparse_clone main https://github.com/kenzok8/small luci-app-daed daed dae
-git_sparse_clone main https://github.com/kenzok8/small luci-app-adguardhome
-git_sparse_clone main https://github.com/kenzok8/small luci-theme-argon luci-app-argon-config
-git_sparse_clone main https://github.com/kenzok8/small luci-app-wolultra 2>/dev/null || true
+# 1. 直接拉取独立插件源码（避免稀疏克隆跳转错误）
+git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
+git clone --depth 1 https://github.com/sbwml/luci-app-daed package/daed
+git clone --depth 1 -b master https://github.com/vernesong/OpenClash package/luci-app-openclash
+git clone --depth 1 https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
 
-# 2. 清理官方 feed 中可能冲突的同名旧插件
+# 2. 清理官方 feed 中可能冲突的旧插件
 rm -rf feeds/luci/themes/luci-theme-argon
 rm -rf feeds/luci/applications/luci-app-argon-config
 rm -rf feeds/luci/applications/luci-app-openclash
 
 # 3. 设置后台管理 IP 为 192.168.1.1 与主机名
-sed -i 's/192.168.[0-9]*.1/192.168.1.1/g' package/base-files/files/bin/config_generate
-sed -i "s/hostname='.*'/hostname='Redmi-AX6'/g" package/base-files/files/bin/config_generate
+[ -f package/base-files/files/bin/config_generate ] && sed -i 's/192.168.[0-9]*.1/192.168.1.1/g' package/base-files/files/bin/config_generate
+[ -f package/base-files/files/bin/config_generate ] && sed -i "s/hostname='.*'/hostname='Redmi-AX6'/g" package/base-files/files/bin/config_generate
 
 # 4. 设置默认 root 密码为空
-sed -i 's/root:::0:99999:7:::/root::0:99999:7:::/g' package/base-files/files/etc/shadow 2>/dev/null || true
+[ -f package/base-files/files/etc/shadow ] && sed -i 's/root:::0:99999:7:::/root::0:99999:7:::/g' package/base-files/files/etc/shadow
 
 # 5. 设置默认 WiFi (2.4G: redmi-ax6-2.4g, 5G: redmiax6-5g, 密码: 123456789)
 mkdir -p package/base-files/files/etc/uci-defaults
