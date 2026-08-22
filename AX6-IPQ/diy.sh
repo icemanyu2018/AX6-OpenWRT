b51c0b6f61c4eed0c0b0ad2b3fcee37731d17510
icemanyu2018 <45337728+icemanyu2018@users.noreply.github.com>
2026-08-22 11:47:52 +0800

    Update diy.sh

--- AX6-IPQ/diy.sh
+++ AX6-IPQ/diy.sh
@@ -9,28 +9,23 @@ function git_sparse_clone() {
   cd .. && rm -rf $repodir
 }
 
-# 1. 添加第三方插件与主题源码
-git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
-git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
+# 1. 从 kenzok8/small 源中按需拉取所需插件
+git_sparse_clone main https://github.com/kenzok8/small luci-app-openclash
+git_sparse_clone main https://github.com/kenzok8/small luci-app-daed daed dae
+git_sparse_clone main https://github.com/kenzok8/small luci-app-adguardhome
+git_sparse_clone main https://github.com/kenzok8/small luci-theme-argon luci-app-argon-config
+git_sparse_clone main https://github.com/kenzok8/small luci-app-wolultra 2>/dev/null || true
 
-# 添加 daed / dae 源码
-git clone --depth 1 https://github.com/sbwml/luci-app-daed package/daed
-
-# 添加 OpenClash 源码
-git clone --depth 1 -b master https://github.com/vernesong/OpenClash package/luci-app-openclash
-
-# 添加 AdGuardHome 插件源码
-git clone --depth 1 https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
-
-# 2. 清理官方 feed 中可能冲突的主题与插件
+# 2. 清理官方 feed 中可能冲突的同名旧插件
 rm -rf feeds/luci/themes/luci-theme-argon
 rm -rf feeds/luci/applications/luci-app-argon-config
 rm -rf feeds/luci/applications/luci-app-openclash
 
-# 3. 设置默认管理 IP: 192.168.1.1
+# 3. 设置后台管理 IP 为 192.168.1.1 与主机名
 sed -i 's/192.168.[0-9]*.1/192.168.1.1/g' package/base-files/files/bin/config_generate
+sed -i "s/hostname='.*'/hostname='Redmi-AX6'/g" package/base-files/files/bin/config_generate
 
-# 4. 设置默认密码为空 (清除 root 默认密码 hash)
+# 4. 设置默认 root 密码为空
 sed -i 's/root:::0:99999:7:::/root::0:99999:7:::/g' package/base-files/files/etc/shadow 2>/dev/null || true
 
 # 5. 设置默认 WiFi (2.4G: redmi-ax6-2.4g, 5G: redmiax6-5g, 密码: 123456789)
@@ -44,7 +39,7 @@ set wireless.@wifi-device[0].disabled='0'
 set wireless.@wifi-device[1].disabled='0'
 UCIBATCH
 
-# 遍历 wifi-iface 进行精确配置
+# 遍历所有无线接口精准配置 SSID 与密码
 for iface in $(uci show wireless | grep '=wifi-iface' | cut -d'.' -f2 | cut -d'=' -f1); do
     device=$(uci -q get wireless.${iface}.device)
     band=$(uci -q get wireless.${device}.band)
