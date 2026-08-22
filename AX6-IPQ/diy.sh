f5c2bd2c5c99ba3c406f0390a7dfdb0db7e48646
icemanyu2018 <45337728+icemanyu2018@users.noreply.github.com>
2026-08-23 02:02:50 +0800

    Update diy.sh

--- AX6-IPQ/diy.sh
+++ AX6-IPQ/diy.sh
@@ -1,24 +1,36 @@
 #!/bin/bash
 
-# 1. 注入第三方源以提供完整的 daed 和 nikki 依赖
-sed -i '$a src-git nikki https://github.com/nikkinikki-org/luci-app-nikki.git;main' feeds.conf.default
-sed -i '$a src-git daed https://github.com/sbwml/luci-app-daed.git;main' feeds.conf.default
-sed -i '$a src-git argon https://github.com/jerrykuku/luci-theme-argon.git;master' feeds.conf.default
+# 1. 彻底清理可能冲突的同名包与旧目录
+rm -rf package/luci-theme-argon package/luci-app-argon-config package/daed package/luci-app-nikki package/nikki
+rm -rf feeds/luci/themes/luci-theme-argon
+rm -rf feeds/luci/applications/luci-app-argon-config
+rm -rf feeds/luci/applications/luci-app-nikki
+rm -rf feeds/luci/applications/luci-app-daed
 
-# 2. 彻底禁用 Rust 编译以防 404
+# 2. 正确拉取独立插件源码到 package 目录
+git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
+
+# 拉取完整包含 dae/daed 核心的源码
+git clone --depth 1 https://github.com/sbwml/luci-app-daed package/daed
+
+# 拉取完整包含 mihomo 核心的 nikki 源码
+git clone --depth 1 https://github.com/nikkinikki-org/luci-app-nikki package/luci-app-nikki
+
+# 3. 彻底禁用 Rust 编译以防 404
 rm -rf feeds/packages/lang/rust
 
-# 3. 设置后台管理 IP 为 192.168.1.1 与主机名
+# 4. 设置后台管理 IP 为 192.168.1.1 与主机名
 [ -f package/base-files/files/bin/config_generate ] && sed -i 's/192.168.[0-9]*.1/192.168.1.1/g' package/base-files/files/bin/config_generate
 [ -f package/base-files/files/bin/config_generate ] && sed -i "s/hostname='.*'/hostname='Redmi-AX6'/g" package/base-files/files/bin/config_generate
 
-# 4. 设置默认 root 密码为空
+# 5. 设置默认 root 密码为空
 [ -f package/base-files/files/etc/shadow ] && sed -i 's/root:::0:99999:7:::/root::0:99999:7:::/g' package/base-files/files/etc/shadow
 
-# 5. 设置默认 WiFi (2.4G: redmi-ax6-2.4g, 5G: redmiax6-5g, 密码: 123456789)
+# 6. 设置默认 WiFi (2.4G: redmi-ax6-2.4g, 5G: redmiax6-5g, 密码: 123456789)
 mkdir -p package/base-files/files/etc/uci-defaults
 cat > package/base-files/files/etc/uci-defaults/99-custom-wifi << 'EOF'
-#!/sh
+#!/bin/sh
 uci -q batch << 'UCIBATCH'
 set wireless.@wifi-device[0].disabled='0'
 set wireless.@wifi-device[1].disabled='0'
